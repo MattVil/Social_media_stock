846f1445f89fbf1f134c51ba3d9a85f74e7a559f
mattvil <mattvilain@gmail.com>
2019-04-03 11:51:19 -0700

    stock output changed

--- stockScraper.py
+++ stockScraper.py
@@ -30,9 +30,19 @@ def get_daily_change(company, date):
 
 def main():
 
-    for i in range(1, 29):
-        print("-"*80)
-        print(get_daily_change("Google", (2, i, 2019)))
+    for i in range(1, 32):
+        print("3/{}/2019 ".format(i)+"-"*70)
+        google = get_daily_change("Google", (3, i, 2019))
+        tesla = get_daily_change("Tesla", (3, i, 2019))
+        apple = get_daily_change("Apple", (3, i, 2019))
+        amazon = get_daily_change("Amazon", (3, i, 2019))
+        facebook = get_daily_change("Facebook", (3, i, 2019))
+        if(google):
+            print("Google: {:.4f}\tTesla: {:.4f}\tApple: {:.4f}\tAmazon: {:.4f}\tFacebook: {:.4f}".format(google,
+                                                                                  tesla,
+                                                                                  apple,
+                                                                                  amazon,
+                                                                                  facebook))
 
 
 if __name__ == '__main__':
